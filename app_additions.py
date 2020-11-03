b076149567c61d9f3c0eae8d2bebd10a194400b0
Rob Atkinson <rob@metalinkage.com.au>
2020-11-03 14:35:00 +1100

    Updates for vp 2.4

--- app_additions.py
+++ app_additions.py
@@ -266,7 +266,8 @@ def index():
 
     parent_child_label_list = []
     seen = {}
-    res = sparql_query(q)
+    
+    res = u.sparql_query(q)
     for r in res:
         p = str(r["parent"]["value"])
         pl = str(r["parentlabel"]["value"])
