50a0730abd72e684be774848433b5ae430abb873
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-11-30 14:16:36 +1000

    VP 2.5

--- apply.sh
+++ apply.sh
@@ -7,18 +7,27 @@ echo "copying $VP_THEME_HOME/templates content to $VP_HOME/vocprez/view/template
 cp $VP_THEME_HOME/templates/* $VP_HOME/vocprez/view/templates
 
 # alter app.py
-echo "replacing index() in $VP_HOME/vocprez/app.py"
-start_line=$(grep -n "# ROUTE index" $VP_HOME/vocprez/app.py | head -n 1 | cut -d: -f1)
-start_line=$((start_line -1))
-head -$start_line $VP_HOME/vocprez/app.py > test.py
+sed -n '/# ROUTE index/q;p' $VP_HOME/vocprez/app.py > $VP_THEME_HOME/app_temp.py
+cat $VP_THEME_HOME/app_additions.py >> $VP_THEME_HOME/app_temp.py
+sed -e '1,/# END ROUTE index/ d' $VP_HOME/vocprez/app.py >> $VP_THEME_HOME/app_temp.py
+mv $VP_THEME_HOME/app_temp.py $VP_HOME/vocprez/app.py
 
-more $VP_THEME_HOME/app_additions.py >> test.py
-
-end_line=$(grep -n "# END ROUTE index" $VP_HOME/vocprez/app.py | head -n 1 | cut -d: -f1)
-end_line=$((end_line + 2))
-tail -n +$end_line $VP_HOME/vocprez/app.py >> test.py
-
-rm $VP_HOME/vocprez/app.py
-mv test.py $VP_HOME/vocprez/app.py
+echo "Config"
+echo "creating VocPrez config with $VP_THEME_HOME/config.py"
+echo "Alter config.py to include variables"
+sed 's#$SPARQL_ENDPOINT#'"$SPARQL_ENDPOINT"'#' $VP_THEME_HOME/config.py > $VP_THEME_HOME/config_updated.py
+if [ -z "$SPARQL_USERNAME" ]
+then
+      sed -i 's#$SPARQL_USERNAME#None#' $VP_THEME_HOME/config_updated.py
+else
+      sed -i 's#$SPARQL_USERNAME#'\"$SPARQL_USERNAME\"'#' $VP_THEME_HOME/config_updated.py
+fi
+if [ -z "$SPARQL_PASSWORD" ]
+then
+      sed -i 's#$SPARQL_PASSWORD#None#' $VP_THEME_HOME/config_updated.py
+else
+      sed -i 's#$SPARQL_PASSWORD#'\"$SPARQL_PASSWORD\"'#' $VP_THEME_HOME/config_updated.py
+fi
+mv $VP_THEME_HOME/config_updated.py $VP_HOME/vocprez/_config/__init__.py
 
 echo "customisation done"
---
+++ config.py
@@ -0,0 +1,55 @@
+from os import path
+
+APP_DIR = path.dirname(path.dirname(path.realpath(__file__)))
+SKIN_DIR = path.join(APP_DIR, "view")
+TEMPLATES_DIR = path.join(SKIN_DIR, "templates")
+STATIC_DIR = path.join(SKIN_DIR, "style")
+LOGFILE = APP_DIR + "/vocprez.log"
+CACHE_FILE = path.join(APP_DIR, "cache", "DATA.p")
+CACHE_HOURS = 1
+DEFAULT_LANGUAGE = "en"
+SPARQL_QUERY_LIMIT = 2000  # Maximum number of results to return per SPARQL query
+MAX_RETRIES = 2
+RETRY_SLEEP_SECONDS = 10
+SPARQL_TIMEOUT = 60
+PORT = 5000
+
+#
+#   Vocabulary data sources
+#
+# Here is the list of vocabulary sources that this instance uses. FILE, SPARQL, RVA & VOCBENCH are implemented already
+# and are on by default (e.g. VOCBENCH = None) but other sources, such as GitHub can be added. To enable them, add a new
+# like like VocBench.XXX = None
+class VocabSource:
+    FILE = "FILE"
+    SPARQL = "SPARQL"
+    RVA = "RVA"
+    VOCBENCH = "VOCBENCH"
+    GITHUB = "GITHUB"
+
+
+# BEGIN Instance Vars
+SYSTEM_URI_BASE = "http://localhost:{}".format(PORT)
+USE_SYSTEM_URIS = True
+DEBUG = True
+SPARQL_ENDPOINT = "$SPARQL_ENDPOINT"
+SPARQL_USERNAME = $SPARQL_USERNAME
+SPARQL_PASSWORD = $SPARQL_PASSWORD
+SOURCE_NAME = "nvs"
+# END Instance Vars
+
+DATA_SOURCES = {
+    # example SPARQL source configured using variables in "Instance Vars" above
+    SOURCE_NAME: {
+        "source": VocabSource.SPARQL,
+        "sparql_endpoint": SPARQL_ENDPOINT,
+        "sparql_username": SPARQL_USERNAME,
+        "sparql_password": SPARQL_PASSWORD,
+    },
+}
+
+# BEGIN Vocabs list info
+VOCS_URI = "https://defs.opengeospatial.org"
+VOCS_TITLE = "OGC Vocabularies"
+VOCS_DESC = "Vocabularies managed by the OGC"
+# END Vocabs list info
