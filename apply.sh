48febed85c9e9e554f92f6f42126438a3dfa06a4
Rob Atkinson <rob@metalinkage.com.au>
2021-03-10 14:57:22 +1100

    set variable defaults for home if unset

--- apply.sh
+++ apply.sh
@@ -1,3 +1,7 @@
+#set defaults to match OGC setup
+VP_THEME_HOME="${VP_THEME_HOME:-/var/www/vocprez_skin}"
+VP_HOME="${VP_HOME:-/var/www/vocprez}"
+
 # copy all style content to VP
 echo "copying $VP_THEME_HOME/style content to $VP_HOME/vocprez/view/style"
 cp $VP_THEME_HOME/style/* $VP_HOME/vocprez/view/style
