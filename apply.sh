1188eb65262db7c1fb942a65159da43e333dd1d5
Rob Atkinson <rob@metalinkage.com.au>
2021-11-08 14:30:22 +1100

    fix typo in last commit

--- apply.sh
+++ apply.sh
@@ -40,7 +40,7 @@ else
       sed -i 's#$SPARQL_PASSWORD#'\"$SPARQL_PASSWORD\"'#' $VP_THEME_HOME/config_updated.py
 fi
 echo $SYSTEM_URI_BASE
-sed -i 's#$SYSTEM_URI_BASE#'E$SYSTEM_URI_BAS'#' $VP_THEME_HOME/config_updated.py
+sed -i 's#$SYSTEM_URI_BASE#'$SYSTEM_URI_BASE'#' $VP_THEME_HOME/config_updated.py
 mv $VP_THEME_HOME/config_updated.py $VP_HOME/vocprez/_config/__init__.py
 
 echo "customisation done"
